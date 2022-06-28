597aaeeedb9f58d99afdb768aab4f3cbace53667
Diyor11 <diyorq3arshi@gmail.com>
2022-06-28 08:54:58 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Navigate, Routes, Route} from 'react-router-dom'
+
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
 
 function App() {
+
+  console.log('first')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <div className='container mt-12 mx-auto py-4'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<Navigate to='/' replace={false} />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
